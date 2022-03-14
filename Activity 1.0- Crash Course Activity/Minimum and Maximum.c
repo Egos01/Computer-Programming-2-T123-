36220ccf2117ab1fe24c9fa72901ef866c271621
TRIXIA GWEN EGOS <100747854+itsme-EGOS@users.noreply.github.com>
2022-03-14 10:00:10 +0800

    Add files via upload

---
+++ Activity 1.0- Crash Course Activity/Minimum and Maximum.c
@@ -0,0 +1,24 @@
+/**
+ *Write a program which reads 10 real numbers. The programmust then show the maximum and the minimum of these numbers*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Enter your numbers: ");
+    int n = 0;
+    scanf ("%d", &n);
+
+    int min=n, max=n;
+    for (int i =1 ; i<= 9; i++)
+    {
+        scanf ("%d", &n);
+
+        min = (n < min) ? n : min;
+        max = (n > max) ? n : max;
+    }
+    printf (" min = %d , max = %d", min, max);
+
+    return 0;
+}
